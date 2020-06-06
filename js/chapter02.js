31b42c41fab7202b070939429374e5e30bdbf3cf
HectorBecMe <hecbecme@gmail.com>
2020-06-06 05:05:30 -0500

    chapter02 - 009
    Variables null undefined and NaN8m 18s

--- js/chapter02.js
+++ js/chapter02.js
@@ -1,6 +1,5 @@
 // VARIABLES BASICS
 console.log('\nVARIABLES BASICS>>>');
-
 // DEFINICION DE VARIABLES
 // Asignacion number.
 // En JavaScript numeros enteros
@@ -26,3 +25,33 @@ console.log(typeof variable);
 var variable1 = 6.5;
 console.log(variable1);
 console.log(typeof variable1);
+
+
+// VARIABLES NULL UNDEFINED AND NAN
+console.log('\nVARIABLES NULL UNDEFINED AND NAN>>>');
+// asignacion de un array
+var variable2 = [1, 2, 3, '4', ];
+console.log(variable2);
+console.log(typeof variable2);
+
+// obtener valor de un array
+console.log(variable2[2]);
+console.log(variable2[4]);
+
+// UNFEFINED
+variable2 = undefined;
+console.log(variable2);
+console.log(typeof variable2);
+
+// NULL vs UNDEFINED
+variable2 = null;
+console.log(variable2);
+console.log(typeof variable2);
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+// NAN
+variable2 = NaN;
+console.log(variable2);
+console.log(typeof variable2);
