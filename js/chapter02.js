6cdabd3a916ecd9f343e189667eedb1c78167218
HectorBecMe <hecbecme@gmail.com>
2020-06-06 04:58:21 -0500

    chapter02 - 008
    Variables Basics6m 48s

---
+++ js/chapter02.js
@@ -0,0 +1,28 @@
+// VARIABLES BASICS
+console.log('\nVARIABLES BASICS>>>');
+
+// DEFINICION DE VARIABLES
+// Asignacion number.
+// En JavaScript numeros enteros
+// y numeros flotantes, son number.
+var variable = 5;
+console.log(variable);
+console.log(typeof variable);
+
+// Asignacion string
+var variable = 't';
+console.log(variable);
+console.log(typeof variable);
+
+// asignacion boolean
+var variable = true;
+console.log(variable);
+console.log(typeof variable);
+
+// Nombre variables no puede comenzar con numero
+// var 1variable = 6.5;
+
+// asignacion de numero flotante
+var variable1 = 6.5;
+console.log(variable1);
+console.log(typeof variable1);
