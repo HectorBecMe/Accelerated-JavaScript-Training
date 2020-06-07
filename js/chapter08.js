faf1d03e8e47f1a2cc5a964bc2b67bbb4b636490
HectorBecMe <hectorbecme@gmail.com>
2020-06-07 18:10:36 -0500

    chapter08 - 076
    The Math Object7m 18s

--- js/chapter08.js
+++ js/chapter08.js
@@ -48,3 +48,31 @@ console.log(string.split(' '));
 console.log(string.trim());
 
 
+// THE MATH OBJECT
+console.log('\nTHE MATH OBJECT>>>');
+var pi = Math.PI;
+console.log(pi);
+
+var e = Math.E;
+console.log(e);
+
+var a = -4;
+console.log(Math.abs(a));
+
+a = 1.99;
+console.log(Math.round(a)); // Redondeo al entero mas CERCANO
+console.log(Math.ceil(a)); // Redondeo al entero SUPERIOR
+console.log(Math.floor(a)); // Redondeo al entero INFERIOR
+
+a = 2;
+console.log(Math.exp(a));
+console.log(Math.log(e));
+
+console.log(Math.max(1,100,1000));
+
+console.log(Math.random());
+
+var rnd = Math.floor(Math.random() * 100) + 1;
+console.log(rnd);
+
+
