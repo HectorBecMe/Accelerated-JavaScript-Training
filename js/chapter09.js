c4fc7be0c649906fbf75d83fdc1d468bcc9da459
HectorBecMe <hectorbecme@gmail.com>
2020-06-07 18:49:37 -0500

    chapter09 - 082
    The Document Object and How to Interact with It7m 18s

--- js/chapter09.js
+++ js/chapter09.js
@@ -24,3 +24,18 @@ console.log(location.pathname);
 //location.replace('https://www.google.com');
 
 
+// THE DOCUMENT OBJECT AND HOW TO INTERACT WITH IT
+console.log('\nTHE DOCUMENT OBJECT AND HOW TO INTERACT WITH IT>>>');
+console.log(document);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.body);
+console.log(document.body.children);
+console.log(document.body.children[0].textContent);
+document.body.children[0].textContent = 'Something else!!';
+document.body.children[0].style.backgroundColor = 'red';
+
+console.log(document.body.children[1].children[0]);
+document.body.children[1].children[0].style.backgroundColor = 'blue';
+
+
