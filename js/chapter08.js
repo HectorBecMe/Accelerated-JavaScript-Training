ce069a710f741f50d55ad88bfaaf9a3958c873d4
HectorBecMe <hectorbecme@gmail.com>
2020-06-07 17:37:07 -0500

    chapter08 - 074
    Transforming Formats Values4m 4s

--- js/chapter08.js
+++ js/chapter08.js
@@ -1,6 +1,6 @@
 // TIMERS INTERVALS
 console.log('\nTIMERS INTERVALS>>>');
-setTimeout(function(){
+/*setTimeout(function(){
   console.log('Timeout NOW!');
 }, 1000);
 
@@ -10,6 +10,27 @@ var interval = setInterval(function(){
 
 setTimeout(function(){
   clearInterval(interval);
-}, 2500);
+}, 2500);*/
+
+
+// TRANFORMING FORMATS VALUES
+console.log('\nTRANFORMING FORMATS VALUES>>>');
+var a = '5';
+console.log(typeof a);
+console.log(a);
+
+console.log(typeof parseInt(a));
+console.log(parseInt(a));
+
+a = 'FBB123';
+console.log(parseInt(a, 16));
+
+a = 10;
+console.log(typeof a.toString());
+console.log(a.toString());
+
+a = 10.3;
+console.log(a.toFixed());
+console.log(a.toFixed(2));
 
 
