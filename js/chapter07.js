7033d1bb09ddcd70ee17513ee6b0340b5b2cd998
HectorBecMe <hectorbecme@gmail.com>
2020-06-07 15:31:27 -0500

    chapter07 - 071
    Built in Methods Properties7m 46s

--- js/chapter07.js
+++ js/chapter07.js
@@ -27,3 +27,19 @@ var obj = {};
 console.log(obj);
 
 
+// BUILT IN METHODS PROPERTIES
+console.log('\nBUILT IN METHODS PROPERTIES>>>');
+function message(message) {
+  console.log(message);
+
+  console.log(arguments); // Arreglo con los parametros de la funcion
+  console.log(arguments[2]);
+  console.log(arguments.length); // Numero de parametros recibidos
+}
+
+message('Hi!', 10, 11, 20);
+console.log('Function name: ' + message.name);
+
+var msg = message;
+console.log('Function name: ' + msg.name); // Nombre de la funcion
+console.log(msg.length); // Numero de parametros esperados
