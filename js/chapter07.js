0e3966318469c200ecb72234e9cde2a9dcff0bee
HectorBecMe <hectorbecme@gmail.com>
2020-06-07 15:13:21 -0500

    chapter07 - 069
    Closures5m 16s

---
+++ js/chapter07.js
@@ -0,0 +1,15 @@
+// CLOSURES
+console.log('\nCLOSURES>>>');
+function generator(input){
+  var number = input;
+  return function() {
+    return number * 2;
+  };
+}
+
+var calc = generator(900);
+var calcAnother = generator(1000);
+console.log(calc);
+console.log(calc());
+
+
