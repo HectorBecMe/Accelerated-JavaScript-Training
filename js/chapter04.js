e355fcc6f7450d76795ad5c0a5cac8ea4219466c
HectorBecMe <hectorbecme@gmail.com>
2020-06-06 23:57:18 -0500

    chapter04 - 041
    Basics Managing Elements3m 34s

---
+++ js/chapter04.js
@@ -0,0 +1,20 @@
+// BASICS MANAGING ELEMENTS
+console.log('\nBASICS MANAGING ELEMENTS>>>');
+var array = [1,2,3];
+
+console.log(array.length);
+
+console.log(array);
+console.log(array[2]);
+console.log(array[3]);
+
+array[1] = 100;
+console.log(array);
+
+array[3] = 33;
+console.log(array);
+
+array[5] = 100;
+console.log(array);
+
+
