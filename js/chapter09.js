ef29152fd42b455a317dea5a0dc5bd086bf7cf96
HectorBecMe <hectorbecme@gmail.com>
2020-06-07 18:33:23 -0500

    chapter09 - 080
    The Window Object7m 23s

---
+++ js/chapter09.js
@@ -0,0 +1,17 @@
+// WINDOW OBJECT
+console.log('\nWINDOW OBJECT>>>');
+console.log(window);
+console.log(window.innerWidth);
+console.log(innerWidth);
+console.log(window.outerWidth);
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+localStorage.setItem('key1', 1000);
+console.log(localStorage.getItem('key1'));
+sessionStorage.setItem('key1', 1000);
+console.log(sessionStorage.getItem('key1'));
+console.log(window.location);
+console.log(window.document);
+//window.open('http://www.google.com');
+
+
