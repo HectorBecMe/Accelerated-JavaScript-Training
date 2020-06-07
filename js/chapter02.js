3792f4d0ee3db516958938871d99373da215a5ce
HectorBecMe <hectorbecme@gmail.com>
2020-06-06 20:48:19 -0500

    chapter02 - 031
    Operators Important Rules3m 1s

--- js/chapter02.js
+++ js/chapter02.js
@@ -540,3 +540,11 @@ console.log(1 >= 1);
 console.log(1 >= '1');
 
 
+// OPERATORS IMPORTANT RULES
+console.log('\nOPERATORS IMPORTANT RULES>>>');
+console.log(NaN == NaN);
+console.log(NaN != NaN);
+console.log(isNaN(NaN));
+console.log(0 == null);
+console.log(null == undefined);
+console.log(0 == undefined);
