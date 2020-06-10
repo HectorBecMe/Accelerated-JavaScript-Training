b31d70eca038f16288b693f3bc64b3ff95fb16cd
HectorBecMe <hectorbecme@gmail.com>
2020-06-10 04:45:33 -0500

    chapter10 - 095
    Event Listeners3m 40s

--- js/chapter10.js
+++ js/chapter10.js
@@ -16,3 +16,23 @@ btn.onclick = function () {
 };
 
 
+// EVENT LISTENERS
+console.log('\nEVENT LISTENERS>>>');
+var btn = document.querySelector('#btn');
+
+function listener1() {
+  console.log('Listener 1...');
+}
+
+function listener2() {
+  console.log('Listener 2...');
+}
+
+btn.addEventListener('click', listener1);
+btn.addEventListener('click', listener2);
+
+setTimeout(function() {
+  btn.removeEventListener('click', listener1);
+}, 5000);
+
+
