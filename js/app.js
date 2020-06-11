765bf369f54a0bdb4092424037ce9be915048475
HectorBecMe <hectorbecme@gmail.com>
2020-06-11 14:36:56 -0500

    chapter13 - 111
    Fetching Handling Weather Data4m 36s

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ searchButton.addEventListener('click', searchWeather);
 function searchWeather() {
   console.log(searchCity.value);
 
-  var cityName = searchCity.value();
+  var cityName = searchCity.value;
   if (cityName.trim().length == 0) {
     return alert('Please enter a City Name');
   }
@@ -14,14 +14,18 @@ function searchWeather() {
   var apikey  = 'c3b811062d9b2c5e20feaa709667f4f7';
 
   url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apikey;
-  var methos = 'GET';
+  var method = 'GET';
 
   http.open(method, url);
   http.onreadystatechange = function() {
     if (http.readyState == XMLHttpRequest.DONE && http.status === 200){
       var data = JSON.parse(http.responseText);
+      var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+      weatherData.temperature = data.main.temp;
+      console.log(weatherData);
     } else if (http.readyState === XMLHttpRequest.DONE) {
       alert('Someting went wrong!');
     }
   };
+  http.send();
 }
